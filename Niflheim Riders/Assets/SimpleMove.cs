5b2dfa7d3690f9452dd57182700fbaccda599032
Toast3y <pyrosburnttoast@gmail.com>
2016-04-25 23:01:36 +0100

    Added models, constructed scene.

---
+++ Niflheim Riders/Assets/SimpleMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+	public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + ((transform.forward * moveSpeed) * Time.deltaTime);
+	}
+}
