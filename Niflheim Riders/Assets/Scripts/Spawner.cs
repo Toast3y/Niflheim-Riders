28b1df5cbf6bf5cca7352c17c9cb3409bacf2479
Toast3y <toast3y@gmail.com>
2016-04-27 01:43:08 +0100

    Added animations, cleaned project.

---
+++ Niflheim Riders/Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	//Spawns an attack force of ships, and randomly promotes one to captain.
+
+	public GameObject ship;
+	public float radius = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+}
---
+++ Niflheim Riders/Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	private bool animating;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//DO A BARREL ROLL!!!
+		if (Input.GetKeyDown("q")) {
+			animator.Play("roll_left");
+		}
+
+		if (Input.GetKeyDown("e")) {
+			animator.Play("roll_right");
+		}
+
+	}
+
+
+}
